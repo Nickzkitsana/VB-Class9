6cc91b30923001610064413e1aa37ff51e15a6d3
kitsana phomgarm <nick7266@hotmail.com>
2019-01-22 16:02:34 +0700

    Complete workshop901

--- workshop901/workshop901/Form1.Designer.vb
+++ workshop901/workshop901/Form1.Designer.vb
@@ -22,10 +22,182 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblcompanyname = New System.Windows.Forms.Label()
+        Me.lblcontactname = New System.Windows.Forms.Label()
+        Me.lbladdress = New System.Windows.Forms.Label()
+        Me.lblcity = New System.Windows.Forms.Label()
+        Me.lblphone = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "กรอกรหัสลูกค้า"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblphone)
+        Me.GroupBox1.Controls.Add(Me.lblcity)
+        Me.GroupBox1.Controls.Add(Me.lbladdress)
+        Me.GroupBox1.Controls.Add(Me.lblcontactname)
+        Me.GroupBox1.Controls.Add(Me.lblcompanyname)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(38, 99)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(563, 282)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "ข้อมูลลูกค้า"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(144, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(330, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(504, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "ค้นหา"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "ชื่อบริษัท"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(54, 102)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "ชื่อผู้ติดต่อ"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(81, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(27, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "ที่อยู่"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(78, 208)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "เมือง"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(36, 255)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(72, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "เบอร์โทรศัพท์"
+        '
+        'lblcompanyname
+        '
+        Me.lblcompanyname.AutoSize = True
+        Me.lblcompanyname.Location = New System.Drawing.Point(137, 50)
+        Me.lblcompanyname.Name = "lblcompanyname"
+        Me.lblcompanyname.Size = New System.Drawing.Size(0, 13)
+        Me.lblcompanyname.TabIndex = 5
+        '
+        'lblcontactname
+        '
+        Me.lblcontactname.AutoSize = True
+        Me.lblcontactname.Location = New System.Drawing.Point(137, 102)
+        Me.lblcontactname.Name = "lblcontactname"
+        Me.lblcontactname.Size = New System.Drawing.Size(0, 13)
+        Me.lblcontactname.TabIndex = 6
+        '
+        'lbladdress
+        '
+        Me.lbladdress.AutoSize = True
+        Me.lbladdress.Location = New System.Drawing.Point(137, 156)
+        Me.lbladdress.Name = "lbladdress"
+        Me.lbladdress.Size = New System.Drawing.Size(0, 13)
+        Me.lbladdress.TabIndex = 7
+        '
+        'lblcity
+        '
+        Me.lblcity.AutoSize = True
+        Me.lblcity.Location = New System.Drawing.Point(137, 208)
+        Me.lblcity.Name = "lblcity"
+        Me.lblcity.Size = New System.Drawing.Size(0, 13)
+        Me.lblcity.TabIndex = 8
+        '
+        'lblphone
+        '
+        Me.lblphone.AutoSize = True
+        Me.lblphone.Location = New System.Drawing.Point(137, 255)
+        Me.lblphone.Name = "lblphone"
+        Me.lblphone.Size = New System.Drawing.Size(0, 13)
+        Me.lblphone.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(641, 413)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "ข้อมูลลูกค้า"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblphone As Label
+    Friend WithEvents lblcity As Label
+    Friend WithEvents lbladdress As Label
+    Friend WithEvents lblcontactname As Label
+    Friend WithEvents lblcompanyname As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- workshop901/workshop901/Form1.vb
+++ workshop901/workshop901/Form1.vb
@@ -1,3 +1,46 @@
-﻿Public Class Form1
+﻿Imports System.Data
+Imports System.Data.SqlClient
 
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim conStr As String
+        Dim id As String = TextBox1.Text
+        Dim found As Boolean = True
+
+        conStr = "Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=D:\Northwind.mdf"
+        Dim conn As New SqlConnection(conStr)
+        conn.Open()
+
+        Dim sql As String = "SELECT CompanyName , ContactName,Address , City , Phone 
+                             FROM Customers
+                             where CustomerID = '" & id.ToUpper & "'"
+        Dim cmd As New SqlCommand(sql, conn)
+        Dim reader As SqlDataReader = cmd.ExecuteReader()
+
+        While reader.Read()
+            lblcompanyname.Text = reader("companyname")
+            lblcontactname.Text = reader("contactname")
+            lbladdress.Text = reader("address")
+            lblcity.Text = reader("city")
+            lblphone.Text = reader("phone")
+            found = False
+        End While
+        If found = True Then
+            MessageBox.Show(id.ToUpper & " not found...")
+            lbladdress.Text = ""
+            lblcontactname.Text = ""
+            lblcity.Text = ""
+            lblphone.Text = ""
+            lblcompanyname.Text = ""
+        End If
+
+        reader.Close()
+        conn.Close()
+
+    End Sub
 End Class
