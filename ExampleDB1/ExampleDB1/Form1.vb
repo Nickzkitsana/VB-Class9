ca5ddef14fc5f8d80636a49bcbac56307a436197
kitsana phomgarm <nick7266@hotmail.com>
2019-01-22 15:26:47 +0700

    Complete Ex

---
+++ ExampleDB1/ExampleDB1/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conStr As String
+        conStr = "Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=D:\Northwind.mdf"
+
+        Dim conn As New SqlConnection(conStr)
+        conn.Open()
+        Dim sql As String = "SELECT * FROM Categories"
+        Dim cmd As New SqlCommand(sql, conn)
+        Dim reader As SqlDataReader = cmd.ExecuteReader
+
+        While reader.Read
+            ListBox1.Items.Add(reader(0) & " : " & reader(1))   'ListBox1.Items.Add(reader("Categoryid") & " : " & reader("categoryname"))
+        End While
+        reader.Close()
+        conn.Close()
+    End Sub
+End Class
---
+++ ExampleDB1/ExampleDB2/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conStr As String
+        conStr = "Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=D:\Northwind.mdf"
+
+        Dim conn As New SqlConnection(conStr)
+        conn.Open()
+        Dim sql As String = "SELECT categoryname , categoryid FROM Categories"
+        Dim cmd As New SqlCommand(sql, conn)
+        Dim adapter As New SqlDataAdapter(cmd)
+        Dim data As New DataSet()
+        adapter.Fill(data, "category")
+
+        'MessageBox.Show(data.Tables("category").Rows.Count)  'Check Rows by Counts
+        'MessageBox.Show(data.Tables("category").Columns(1).ToString)  'Check Rows by Counts
+
+        DataGridView1.DataSource = data.Tables("category")
+        For i = 0 To data.Tables("category").Rows.Count - 1
+            ListBox1.Items.Add(data.Tables("category").Rows(i)("categoryname"))
+        Next
+
+        conn.Close()
+    End Sub
+End Class
